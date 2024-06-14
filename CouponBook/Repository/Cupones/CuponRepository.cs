8ccb4a7a043bfb2f6e5698b6875332607ae5d5a7
CristhianDavid0207 <emmacris04@gmail.com>
2024-06-14 13:46:36 -0500

    feat: Agregando estructura de carpetas y archivos

---
+++ CouponBook/Repository/Cupones/CuponRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CouponBook.Repository.Cupones
+{
+    public class CuponRepository
+    {
+        
+    }
+}
